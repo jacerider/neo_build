2d7fcb5c068a20cf73dece1a63e0952bd997f428
JaceRider <jacerider@gmail.com>
2025-09-19 15:54:59 -0500

    ♻️ (neo-vite.cjs): remove commented-out code and simplify build configuration for clarity and maintainability

--- tools/neo-vite.cjs
+++ tools/neo-vite.cjs
@@ -5,7 +5,6 @@ module.exports = {
 
   buildConfig: (neo) => {
     let config = {
-      // outDir: './themes/tmp/dist',
       outDir: './' + neo.primaryRoot + '/dist',
       cssCodeSplit: true,
       manifest: 'manifest.json',
@@ -17,29 +16,8 @@ module.exports = {
           ],
         }
       },
-      // rollupOptions: {
-      //   output: {
-      //     assetFileNames: (entry) => {
-      //       console.log('assetFileNames');
-      //       return '[name].[ext]';
-      //     },
-      //     chunkFileNames: (entry) => {
-      //       console.log('chunkFileNames');
-      //       return "[name].js";
-      //     },
-      //     entryFileNames: (entry) => {
-      //       console.log('entryFileNames');
-      //       return "[name].js";
-      //     }
-      //   }
-      // }
     };
-    if (process.env.NODE_ENV === 'development') {
-      // config.rollupOptions = {
-      //   input: neo.vite.lib,
-      // };
-    }
-    else {
+    if (process.env.NODE_ENV !== 'development') {
       config.lib = {
         entry: neo.vite.lib,
       };
@@ -57,31 +35,6 @@ module.exports = {
         origin: /https?:\/\/([A-Za-z0-9\-\.]+)?(\.ddev\.site)(?::\d+)?$/,
       }
     }
-    return {
-      host: '0.0.0.0',
-      port: 5173,
-      strictPort: true,
-      origin: `${process.env.DDEV_PRIMARY_URL.replace(/:\d+$/, "")}:${neo.port}`,
-      cors: {
-        origin: /https?:\/\/([A-Za-z0-9\-\.]+)?(\.ddev\.site)(?::\d+)?$/,
-      }
-    };
-    let origin = '';
-    if (process.env.DDEV_PRIMARY_URL) {
-      neo.host = '0.0.0.0';
-      origin = `${process.env.DDEV_PRIMARY_URL}:${neo.port}`;
-      neo.https = false;
-    }
-    return {
-      host: neo.host,
-      origin: origin,
-      strictPort: true,
-      port: neo.port,
-      https: neo.https,
-      watch: {
-        ignored: neo.ignored,
-      },
-    }
-
   }
+
 };
